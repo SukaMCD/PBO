e6a0f196dee89b86ffdbffab7079e386bacaa57c
SukaMCD <fabian25march@gmail.com>
2025-08-04 13:47:25 +0700

    funcparam

--- FuncParam.js
+++ FuncParam.js
@@ -40,20 +40,42 @@
 // car.drive();
 
 //Latihan
-let sepeda = {
-    merk: "Polygon",
-    warna: "Merah",
-    jumlah_gear: "7",
+// let sepeda = {
+//     merk: "Polygon",
+//     warna: "Merah",
+//     jumlah_gear: "7",
 
-    start : function() {
-        console.log("Sepeda mulai dikayuh");
-    },
-    brake : function() {
-        console.log("Sepeda mengerem");
-    },
-};
+//     start : function() {
+//         console.log("Sepeda mulai dikayuh");
+//     },
+//     brake : function() {
+//         console.log("Sepeda mengerem");
+//     },
+// };
 
-console.log(sepeda.merk);
-console.log(sepeda.warna);
-sepeda.start();
-sepeda.brake();
+// console.log(sepeda.merk);
+// console.log(sepeda.warna);
+// sepeda.start();
+// sepeda.brake();
+
+//func
+// function car(name, model, weight, color) {
+//     this.name = name;
+//     this.model = model;
+//     this.weight = weight;
+//     this.color = color;
+
+//     this.start = function() {
+//         console.log(`${this.name} dinyalakan`);
+//     };
+//     this.drive = function() {
+//         console.log(`${this.name} berjalan`);
+//     };
+// }
+
+// let car1 = new car("Toyota", "Corolla", "1300kg", "blue");
+// let car2 = new car("Honda", "Civic", "1200kg", "red");
+
+// car1.start();
+// car2.drive();
+
