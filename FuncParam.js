38409a402334d446c2c0691b88f924fb92dc033f
SukaMCD <fabian25march@gmail.com>
2025-08-04 15:01:42 +0700

    funcparam

--- FuncParam.js
+++ FuncParam.js
@@ -79,3 +79,74 @@
 // car1.start();
 // car2.drive();
 
+// class Car {
+//     constructor(name, model, weight, color) {
+//         this.name = name;
+//         this.model = model;
+//         this.weight = weight;
+//         this.color = color;
+//     }
+
+//     start() {
+//         console.log(`${this.name} dinyalakan`);
+//     }
+
+//     drive() {
+//         console.log(`${this.name} berjalan`);
+//     }
+
+//     brake() {
+//         console.log(`${this.name} mengerem`);
+//     }
+
+//     stop() {
+//         console.log(`${this.name} berhenti`);
+//     }
+// }
+// let car1 = new Car("Toyota", "Corolla", "1300kg", "blue");
+// let car2 = new Car("Honda", "Civic", "1200kg", "red");
+// car1.start();
+
+//latihan class
+// class sepeda {
+//     constructor(merk, warna, jumlah_gear) {
+//         this.merk = merk;
+//         this.warna = warna;
+//         this.jumlah_gear = jumlah_gear;
+//     }
+
+//     start() {
+//         console.log(`${this.merk} mulai dikayuh`);
+//     }
+
+//     brake() {
+//         console.log(`${this.merk} mengerem`);
+//     }
+// }
+// let sepeda1 = new sepeda("Polygon", "Merah", 7);
+// let sepeda2 = new sepeda("United", "Biru", 21);
+
+// sepeda1.start();
+// sepeda2.brake();
+
+//latihan lagi
+class Student {
+  constructor(nama, nim, jurusan, tahun) {
+    this.nama = nama;
+    this.nim = nim;
+    this.jurusan = jurusan;
+    this.tahun =- tahun;
+  }
+
+  introduce() {
+    console.log(`Halo saya ${this.nama}, mahasiswa ${this.jurusan} angkatan ${this.tahun}`);
+  }
+
+  isSenior() {
+    return this.tahun < 2025;
+  }
+}
+
+const mhs1 = new Student("Fabian", "12345678", "Informatika", 2026);
+mhs1.introduce();
+console.log(mhs1.isSenior());
